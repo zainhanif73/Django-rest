bf0b14948be386ee38061db30b2c3b5e62ca2614
ZAINHANIF <www.zainhanif73@gmail.com>
2022-06-05 14:33:18 +0500

    added

--- gs5/settings.py
+++ gs5/settings.py
@@ -79,12 +79,8 @@ WSGI_APPLICATION = 'gs5.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'django-rest-table',
-        'USER' : 'root',
-        'PASSWORD' : '',
-        'HOST' : '127.0.0.1',
-        'PORT' : '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'mydatabase',
     }
 }
 
